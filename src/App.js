4f034416a93a5cfb09813f8561cd02d72d649dd2
ccharles78 <charaycharles@gmail.com>
2019-01-27 13:48:25 -0500

    new

--- src/App.js
+++ src/App.js
@@ -2,57 +2,57 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
-import FriendCard from "./components/FriendCard";
+import sharkCard from "./components/sharkCard";
 import Footer from "./components/Footer";
-import fish from "./fish.json";
+import sharks from "./sharks.json";
 import "./App.css";
 
 //sets state to 0 or empty
 class App extends Component {
   state = {
-    fish,
-    clickedFish: [],
+    sharks,
+    clickedsharks: [],
     score: 0
   };
 
-//when you click on a card ... the fish is taken out of the array
+//when you click on a card ... the sharks is taken out of the array
   imageClick = event => {
-    const currentFish = event.target.alt;
-    const FishAlreadyClicked =
-      this.state.clickedFish.indexOf(currentFish) > -1;
+    const currentsharks = event.target.alt;
+    const sharksAlreadyClicked =
+      this.state.clickedsharks.indexOf(currentsharks) > -1;
 
-//if you click on a fish that has already been selected, the game is reset and cards reordered
-    if (FishAlreadyClicked) {
+//if you click on a sharks that has already been selected, the game is reset and cards reordered
+    if (sharksAlreadyClicked) {
       this.setState({
-        fish: this.state.fish.sort(function(a, b) {
+        sharks: this.state.sharks.sort(function(a, b) {
           return 0.5 - Math.random();
         }),
-        clickedFish: [],
+        clickedsharks: [],
         score: 0
       });
         alert("You lose. Play again?");
 
-//if you click on an available fish, your score is increased and cards reordered
+//if you click on an available sharks, your score is increased and cards reordered
     } else {
       this.setState(
         {
-          fish: this.state.fish.sort(function(a, b) {
+          sharks: this.state.sharks.sort(function(a, b) {
             return 0.5 - Math.random();
           }),
-          clickedFish: this.state.clickedFish.concat(
-            currentFish
+          clickedsharks: this.state.clickedsharks.concat(
+            currentsharks
           ),
           score: this.state.score + 1
         },
-//if you get all 12 fish corrent you get a congrats message and the game resets        
+//if you get all 12 sharks corrent you get a congrats message and the game resets        
         () => {
           if (this.state.score === 12) {
             alert("Yay! You Win!");
             this.setState({
-              fish: this.state.fish.sort(function(a, b) {
+              sharks: this.state.sharks.sort(function(a, b) {
                 return 0.5 - Math.random();
               }),
-              clickedFish: [],
+              clickedsharks: [],
               score: 0
             });
           }
@@ -61,7 +61,7 @@ class App extends Component {
     }
   };
 
-//the order of components to be rendered: navbar, jumbotron, friendcard, footer 
+//the order of components to be rendered: navbar, jumbotron, sharkcard, footer 
   render() {
     return (
       <div>
@@ -70,12 +70,12 @@ class App extends Component {
         />
         <Jumbotron />
         <div className="wrapper">
-          {this.state.fish.map(fish => (
-            <FriendCard
+          {this.state.sharks.map(sharks => (
+            <sharkCard
               imageClick={this.imageClick}
-              id={fish.id}
-              key={fish.id}
-              image={fish.image}
+              id={sharks.id}
+              key={sharks.id}
+              image={sharks.image}
             />
           ))}
         </div>
